88b269d52a531829116c195dee3b87c5ae309b11
Florian CATINAUD <fcatinau@e3r8p12.clusters.42paris.fr>
2022-05-10 15:52:56 +0200

    Add gitignore and continue Stack

--- Stack.hpp
+++ Stack.hpp
@@ -4,18 +4,52 @@
 # include <vector>
 # include <algorithm>
 
-namespace ft{
-	template <class T, class Container = std::vector<T> > class stack{ // modif std::vector for ft::vector
-		private :
-			T			data;
+namespace ft {
+template <class T, class Container = std::vector<T> >
+	class stack {
+		public:
+			typedef typename Container::value_type	value_type;
+			typedef typename Container::size_type	size_type;
+			typedef Container						container_type;
+		protected:
+			Container c;
+		public:
+			explicit stack(const Container& = Container()) : c() {};
 
-		public :
-			stack(void){};
-			~stack(){};
+			bool empty() const { return c.empty(); }
 
-	}
+			size_type size() const { return c.size(); }
+
+			void push(const value_type& x) { c.push_back(x); }
+
+			value_type& top() { return c.back(); }
+			// const value_type& top() const { return c.back(); }
+
+			void pop() { c.pop_back(); }
+	};
+	// Need to reimplement ?
+	template <class T, class Container>
+	bool operator==(const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator< (const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator!=(const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator> (const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator>=(const stack<T, Container>& x,
+	const stack<T, Container>& y);
+	template <class T, class Container>
+	bool operator<=(const stack<T, Container>& x,
+	const stack<T, Container>& y);
 }
-
 // https://www.guru99.com/stack-in-cpp-stl.html
 // https://fr.acervolima.com/std-allocator-en-c-avec-des-exemples/#:~:text=allocator%20est%20l%27allocateur%20de,pour%20au%20moins%20n%20%C3%A9l%C3%A9ments
 #endif
+
+// https://www.lirmm.fr/~ducour/Doc-objets/ISO+IEC+14882-1998.pdf
+// page 485
--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include <stack>
 #include "Stack.hpp"
+#include <iostream>
+#include <algorithm>
 
+// STACK MAIN
 int	main(void)
 {
 	std::stack<int>	real;
 	ft::stack<int>	mind;
 
+	std::cout << "Est ce vide ?" << std::endl;
+	std::cout << "real = " << real.empty() << std::endl;
+	std::cout << "mind = " << mind.empty() << std::endl;
+
+	std::cout << "Je push 5" << std::endl;
+	real.push(5);
+	mind.push(5);
+
+	std::cout << "Est ce vide ?" << std::endl;
+	std::cout << "real = " << real.empty() << std::endl;
+	std::cout << "mind = " << mind.empty() << std::endl;
+
+	std::cout << "Je push 6" << std::endl;
+	real.push(6);
+	mind.push(6);
+
+	std::cout << "Je push 7" << std::endl;
+	real.push(7);
+	mind.push(7);
+
+	std::cout << "Qu'elle est mon top ?" << std::endl;
+	std::cout << "real = " << real.top() << std::endl;
+	std::cout << "mind = " << mind.top() << std::endl;
+
+	std::cout << "J'utilise pop" << std::endl;
+	real.pop();
+	mind.pop();
+
+	std::cout << "Qu'elle est mon top ?" << std::endl;
+	std::cout << "real = " << real.top() << std::endl;
+	std::cout << "mind = " << mind.top() << std::endl;
+
+	std::cout << "Combien j'ai d'elem ?" << std::endl;
+	std::cout << "real = " << real.size() << std::endl;
+	std::cout << "mind = " << mind.size() << std::endl;
+
 	return (EXIT_SUCCESS);
 }
+// END STACK MAIN
