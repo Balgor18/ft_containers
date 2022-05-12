60be7e5e1881b98f1db964e792be79b2dcce4f40
Florian CATINAUD <fcatinau@e3r6p9.clusters.42paris.fr>
2022-05-12 18:14:23 +0200

    Some little modif

---
+++ Makefile
@@ -0,0 +1,26 @@
+NAME	=	container
+
+SRCS	=	main.cpp
+OBJS	=	$(SRCS:%.cpp=%.o)
+DEPS	=	$(OBJS:%.o=%.d)
+
+CXX		=	clang++
+CXXFLAGS	=	-Wall -Wextra -Werror -MMD -MP
+
+
+all		:	$(NAME)
+
+$(NAME)	:	$(OBJS)
+	${CXX} ${CXXFLAGS} -o ${NAME} ${OBJS}
+
+-include ${DEP}
+%.o		:	%.cpp
+	${CXX} ${CXXFLAGS} -o $@ -c $<
+
+clean	:
+	rm -rf $(OBJS)
+
+fclean	:	clean
+	rm -rf $(NAME)
+
+re		:	fclean all
--- Stack.hpp
+++ Stack.hpp
@@ -48,6 +48,8 @@ template <class T, class Container = std::vector<T> >
 }
 // https://www.guru99.com/stack-in-cpp-stl.html
 // https://fr.acervolima.com/std-allocator-en-c-avec-des-exemples/#:~:text=allocator%20est%20l%27allocateur%20de,pour%20au%20moins%20n%20%C3%A9l%C3%A9ments
+// https://h-deb.clg.qc.ca/Sujets/Divers--cplusplus/SFINAE.html
+
 #endif
 
 // https://www.lirmm.fr/~ducour/Doc-objets/ISO+IEC+14882-1998.pdf
